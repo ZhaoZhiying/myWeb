75f62c5e9bafef2cc9154433ce05ff1c742713ed
ZhaoZhiYing <31733268+ZhaoZhiYing@users.noreply.github.com>
2018-01-17 15:32:01 +0800

    添加 view controller

--- js/init-swiper.js
+++ js/init-swiper.js
@@ -1,12 +1,28 @@
 !function(){
-    var mySwiper = new Swiper ('.swiper-container', {
-        loop: true, //是否无缝
-        pagination: {
-        el: '.swiper-pagination'//是否需要分页符
-    },
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+    var view = document.querySelector('#mySlides')
+    var controller = {
+        view: null,
+        swiper: null,
+        swiperOptions: {
+            loop: true, //是否无缝
+                pagination: {
+                    el: '.swiper-pagination'//是否需要分页符
+                },
+                navigation: {
+                    nextEl: '.swiper-button-next',
+                    prevEl: '.swiper-button-prev',
+                },
         },
-    })
+        init: function(view){
+            this.view = view
+            this.initSwiper()
+        },
+        initSwiper: function(){
+            this.swiper = new Swiper (
+                view.querySelector('.swiper-container'),
+                this.swiperOptions
+            )
+        }
+    }    
+    controller.init(view)
 }.call()
--- js/sticky-topbar.js
+++ js/sticky-topbar.js
@@ -1,9 +1,28 @@
 !function(){
-    window.addEventListener('scroll', function(x){
-        if(window.scrollY > 0){//获取页面滚动时的高度
-            header.classList.add('sticky')//滚动时加sticky
-        }else{
-            header.classList.remove('sticky')
-        }
-    })
+    var view = document.querySelector('#header')
+    
+    var controller = {
+        view: null,
+        init: function(view){
+                this.view = view
+                this.bindEvents() // 等价于 this.bindEvents.call(this)
+              },
+        bindEvents: function(){
+        var view = this.view
+        window.addEventListener('scroll', (x)=>{ // 箭头函数没有 this
+            if(window.scrollY > 0){ //获取页面滚动时的高度
+                this.active() //滚动时加sticky
+            }else{
+                this.deactive()
+            }
+        })
+        },
+        active: function(){ 
+            this.view.classList.add('sticky')
+        },
+        deactive: function(){ 
+        this.view.classList.remove('sticky')
+    }
+    }
+  controller.init(view) // 等价于 controller.init.call(controller, view)
 }.call()

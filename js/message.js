0ce2bc4a43ff731cbd53a2a2d3abfb841b88617b
ZhaoZhiYing <31733268+ZhaoZhiYing@users.noreply.github.com>
2018-01-18 23:29:23 +0800

    给简历加个数据库-获取历史留言

--- js/message.js
+++ js/message.js
@@ -7,15 +7,42 @@ AV.init({
   appKey: APP_KEY
 });
 
-postMessageForm.addEventListener('submit', function(e){
+var query = new AV.Query('Message');
+query.find()
+.then(
+    function (messages) {
+    let array = messages.map((item)=>item.attributes)
+    array.forEach((item)=>{
+        let li = document.createElement('li')
+        li.innerText = item.content
+        let messageList = document.querySelector('#messageList')
+        messageList.appendChild(li)
+    })
+}, 
+    function (error) {
+    // 异常处理
+    alert('改天再来哟')
+    }
+)
+.then(// then 就是成功调用第一个函数，失败调用第二个函数
+   ()=>{},
+   (error)=>{
+       console.log(error)
+   }
+)
+
+
+let myForm = document.querySelector('#postMessageForm')
+
+myForm.addEventListener('submit', function(e){
     e.preventDefault()
-    let content = postMessageForm.querySelector('input[name = content]').value
+    let content = myForm.querySelector('input[name = content]').value
     var Message = AV.Object.extend('Message');
     var message = new Message();
     message.save({
         'content': content
     }).then(function(object){
-        alert('存入成功')
+        window.location.reload()//自动刷新
         console.log(object)
     })
 })

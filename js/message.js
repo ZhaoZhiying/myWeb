b9928d2632051d5c7123bcf89cf756a874a1e5e9
ZhaoZhiYing <31733268+ZhaoZhiYing@users.noreply.github.com>
2018-01-19 13:36:05 +0800

    给简历加个数据库-无刷新留言

--- js/message.js
+++ js/message.js
@@ -9,26 +9,16 @@ AV.init({
 
 var query = new AV.Query('Message');
 query.find()
-.then(
+.then(// then 就是成功调用第一个函数，失败调用第二个函数
     function (messages) {
     let array = messages.map((item)=>item.attributes)
     array.forEach((item)=>{
         let li = document.createElement('li')
-        li.innerText = item.content
+        li.innerText = `${item.name}: ${item.content}`
         let messageList = document.querySelector('#messageList')
         messageList.appendChild(li)
     })
-}, 
-    function (error) {
-    // 异常处理
-    alert('改天再来哟')
-    }
-)
-.then(// then 就是成功调用第一个函数，失败调用第二个函数
-   ()=>{},
-   (error)=>{
-       console.log(error)
-   }
+  }
 )
 
 
@@ -36,13 +26,21 @@ let myForm = document.querySelector('#postMessageForm')
 
 myForm.addEventListener('submit', function(e){
     e.preventDefault()
+    let name = myForm.querySelector('input[name = name]').value
     let content = myForm.querySelector('input[name = content]').value
     var Message = AV.Object.extend('Message');
     var message = new Message();
     message.save({
+        'name': name,
         'content': content
     }).then(function(object){
-        window.location.reload()//自动刷新
+        //无刷新留言
+        let li = document.createElement('li')
+        li.innerText = `${object.attributes.name}: ${object.attributes.content}`
+        let messageList = document.querySelector('#messageList')
+        messageList.appendChild(li)
+        myForm.querySelector('input[name = name]').value = ''//自动清空输入框内容 
+        myForm.querySelector('input[name = content]').value = ''
         console.log(object)
     })
 })

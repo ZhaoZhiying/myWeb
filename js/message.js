a1c4d1d41e5b491c13853473a51303e1b810c38b
ZhaoZhiYing <31733268+ZhaoZhiYing@users.noreply.github.com>
2018-03-14 11:18:56 +0800

    增加 input 输入判断

--- js/message.js
+++ js/message.js
@@ -21,36 +21,34 @@ query.find()
   }
 )
 
-
 let myForm = document.querySelector('#postMessageForm')
 
 myForm.addEventListener('submit', function(e){
     e.preventDefault()
     let name = myForm.querySelector('input[name = name]').value
     let content = myForm.querySelector('input[name = content]').value
-    var Message = AV.Object.extend('Message');
-    var message = new Message();
+    //判断 input 输入情况
+    if(content !== '' && name !== ''){
+        var Message = AV.Object.extend('Message');
+        var message = new Message();
+    }else if(name === ''){
+        alert('还不知道怎么称呼你呢')
+    }else if(content === ''){
+        alert('写点什么吧')
+    }
     message.save({
         'name': name,
         'content': content
     }).then(function(object){
+        alert('发送成功')
         //无刷新留言
         let li = document.createElement('li')
         li.innerText = `${object.attributes.name}: ${object.attributes.content}`
         let messageList = document.querySelector('#messageList')
         messageList.appendChild(li)
-        myForm.querySelector('input[name = name]').value = ''//自动清空输入框内容 
+        //自动清空输入框内容
+        myForm.querySelector('input[name = name]').value = '' 
         myForm.querySelector('input[name = content]').value = ''
         console.log(object)
     })
 })
-
-/* 
-var TestObject = AV.Object.extend('TestObject');
-var testObject = new TestObject();
-testObject.save({
-  words: 'Hello World!'
-}).then(function(object) {
-  alert('LeanCloud Rocks!');
-})
-*/
